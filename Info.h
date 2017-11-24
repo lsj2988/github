43fd16e2b98966408b617947dc26a30aac24ba56
lsj2988 <tpwls2988@naver.com>
2017-11-24 15:42:26 +0900

    project

---
+++ Info.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_P 10
+#define MAX_N 80
+
+typedef struct information {
+    char name[20];
+    char id[20];
+    char psw[20];
+    char sch[20];
+    char city[20];
+    char age[4];
+    char title[80][20];
+    char memo[80][200];
+    char stime[80][30];
+}info;
+
+info arr[10];
+int npnum = 0; // For New User Number
+int pnum = 0; // Now User Number
+int osnum = 0; // Occupied User
+int tnum[10] = {0,}; // Memo Number fo Each User 
