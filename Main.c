44f1dc57c59019d59ce23cc5f0c0af1f64cf67b5
lsj2988 <tpwls2988@naver.com>
2017-12-03 15:45:24 +0900

    project

--- Main.c
+++ Main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "data1.h"
+#include "data.h"
 
 int  main(void) {
     int i = 0;
     int j = 0;
-    Sign_up(arr, &pnum1);
-
+    Sign_up(arr, &pnum);
+    system("clear");
     printf("---------------------------\n");
     system("clear");
     printf("Name : %s\n",arr[i].name);
@@ -16,7 +16,7 @@ int  main(void) {
     printf("City : %s\n",arr[i].city);
     printf("Age : %s\n",arr[i].age);
     
-    To_write_note(arr,&pnum2,&pnum3);
+    To_write_note(arr,&pnum1,&tnum[pnum1]);
     printf("Title : %s\n",arr[i].title[j]);
     printf("Memo : %s\n",arr[i].memo[j]);
 
